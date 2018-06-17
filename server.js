2978cf352b4e320b35b3d2c233987c4e37b60ea7
Paul Kelly <paul.kelly20@gmail.com>
2018-06-17 10:34:12 +0100

    Commit after moving buttons

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
+
 
 app.use(express.static('public'));
 
